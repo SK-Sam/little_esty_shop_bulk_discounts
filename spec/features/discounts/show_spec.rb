16cbdb99bd1166d059eb3d1f7a7d9e915649ada4
Sam Yeo <yeosamuel95@gmail.com>
2021-01-19 12:29:57 -0700

    Add test which checks for existence of edit link

--- spec/features/discounts/show_spec.rb
+++ spec/features/discounts/show_spec.rb
@@ -2,13 +2,17 @@ require 'rails_helper'
 
 RSpec.describe 'Show Page for Merchant Discounts' do
   describe 'Individual page for each Merchant Discount' do
+    before :each do
+      @merchant1 = Merchant.create!(name: "Test1")
+      @discount1 = merchant1.discounts.create!(threshold: 20, percent: 10)
+      visit merchant_discount_path(@merchant1, @discount1)
+    end
     it 'can display a specific discount quantity threshold and percentage discount' do
-      merchant1 = Merchant.create!(name: "Test1")
-      discount1 = merchant1.discounts.create!(threshold: 20, percent: 10)
-      visit merchant_discount_path(merchant1, discount1)
-      
-      expect(page).to have_content("Items must reach #{discount1.threshold} threshold to receive discount")
-      expect(page).to have_content("Items will receive #{discount1.percent}% off if threshold is reached")
+      expect(page).to have_content("Items must reach #{@discount1.threshold} threshold to receive discount")
+      expect(page).to have_content("Items will receive #{@discount1.percent}% off if threshold is reached")
+    end
+    it 'has a link that leads to an edit page' do
+      expect(page).to have_content "Edit Discount"
     end
   end
 end
